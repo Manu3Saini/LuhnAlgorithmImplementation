a860b33f5d320aa8089018be99f580ef2a2f5958
Manu Saini <manu3saini@gmail.com>
2020-10-19 14:38:59 +1000

    changing annotation to RestController

--- src/main/java/com/example/luhnalgo/CardValidationController.java
+++ src/main/java/com/example/luhnalgo/CardValidationController.java
@@ -1,16 +1,15 @@
 package com.example.luhnalgo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CardValidationController {
 
 
-    @RequestMapping(value = "/validate/{cardnumber}", method = RequestMethod.GET)
+    @RequestMapping(value = "/validate/{cardnumber}")
     public @ResponseBody
     ValidationResult isValidCardNumber(@PathVariable("cardnumber") String cardnumber) {
         CardValidationService service = new CardValidationService(cardnumber);
